7fa9a1b2b3201c32b18411c0c55dcaed1a4c4ed6
Batrach <63921506+Batrach@users.noreply.github.com>
2022-12-26 01:50:47 +0300

    Update mail.php

--- mail.php
+++ mail.php
@@ -14,7 +14,7 @@ $mail->isSMTP();                                      // Set mailer to use SMTP
 $mail->Host = 'smtp.mail.ru';  																							// Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
 $mail->Username = 'ababakumkum@mail.ru'; // Ваш логин от почты с которой будут отправляться письма
-$mail->Password = 'MCdm6tP7sf0VbiVpYiLd'; // Ваш пароль от почты с которой будут отправляться письма
+$mail->Password = 'Rh6WQmqintgALHedX0eQ'; // Ваш пароль от почты с которой будут отправляться письма
 $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 465; // TCP port to connect to / этот порт может отличаться у других провайдеров
 
